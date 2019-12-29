b1e8a324c46ee3b39c8a2196a048fa736ef01e46
Alexander <trailblazer2006@yandex.ru>
2019-12-29 21:02:41 +0300

    update packages

--- src/app/components/index.ts
+++
@@ -1,3 +0,0 @@
-import { WordCardEditComponent } from './word-card-edit/word-card-edit.component';
-
-export { WordCardEditComponent };
--- src/app/models/index.ts
+++
@@ -1,5 +0,0 @@
-import { Dictionary } from './dictionary.model';
-import { User } from './user.model';
-import { Word } from './word.model';
-
-export { Dictionary, User, Word };
