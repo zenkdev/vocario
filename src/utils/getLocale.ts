f3023647e044cef34c3c4ad1315de65fbdd57cd3
Alexander <trailblazer2006@yandex.ru>
2020-04-11 13:46:30 +0300

    added chart

---
+++ src/utils/getLocale.ts
@@ -0,0 +1,14 @@
+import { Locale } from 'date-fns';
+import enUS from 'date-fns/locale/en-US';
+import ru from 'date-fns/locale/ru';
+
+const locales: Record<string, Locale> = {
+  'en-US': enUS,
+  ru,
+};
+
+export default function getLocale(name?: string): Locale {
+  const { language } = window.navigator;
+  const ensureName = name || language || 'en';
+  return locales[ensureName] || ru;
+}
