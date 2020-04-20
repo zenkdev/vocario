a1018a519816b376e3271870322cd122308c41ae
Alexander <trailblazer2006@yandex.ru>
2020-04-20 21:01:40 +0300

    fixed merge confilict

--- src/features/app/PrivateRoute.tsx
+++ src/features/app/PrivateRoute.tsx
@@ -4,13 +4,8 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { Route, RouteProps } from 'react-router-dom';
 
-<<<<<<< HEAD:src/features/app/PrivateRoute.tsx
 import { RootState } from '../../app/rootReducer';
 import { Login } from '../../pages';
-=======
-import { RootState } from './rootReducer';
-import { Login } from '../pages';
->>>>>>> 291d029... moved private router close to usage:src/app/PrivateRoute.tsx
 
 const PrivateRoute: React.FC<RouteProps> = ({ component: Component, ...rest }) => {
   const { isLoggedIn } = useSelector((state: RootState) => state.app);
